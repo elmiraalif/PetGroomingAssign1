b944dc930a4a4cc5ebadf69a03149666635064e8
Elmira Alif <elmira.alif@gmail.com>
2020-01-23 14:42:04 -0500

    My First commit

---
+++ PetGrooming/Models/GroomService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetGrooming.Models
+{
+    public class GroomService
+    {
+        /*
+            A GroomService is a type of activity that a Groomer can do for a pet. Some examples might be nail clipping, shampoo, trim, etc.
+            
+            Some things that describe a GroomService
+                - Service Name
+                - Cost
+                - Duration
+         */
+            [key]
+            public int ServiceID { get; set; }
+            public string ServiceName { get; set; }
+            /*The duration is going to be stored in MINUTES (ex. 1.5 hr  = 90 min) */
+            public int ServiceDuration { get; set; }
+            /*The cost is going to be stored in CENTS (ex. $20  = 2000 cents) */
+            public int ServiceCost { get; set; }
+
+    }
+}
---
+++ PetGrooming/Models/Owner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetGrooming.Models
+{
+    public class Owner
+    {
+        /*
+            An owner is someone who owns one or more pets
+            Some things that describe an owner:
+                - First Name
+                - Last Name
+                - Address
+                - Phone Number (work)
+                - Phone Number (home)
+              
+
+            An owner must reference a list of pets
+            
+        */
+        [key]
+        public int OwnerID { get; set; }
+        /* Name of the Owner is a composite attribute containing the attributes below : */
+        public string OwnerFName { get; set; }
+        public string OwnerLName { get; set; }
+
+        public string OwnerEmail { get; set; }
+        public string Ownerphone { get; set; }
+        /* Owner has an attribute of address but address is a composite attribute 
+         which means it includes other attributes,
+         instead of defining OwnerAdress we have these 3 attributes: */
+        public string OwnerCity { get; set; }
+        public string OwnerStreet { get; set; }
+        public string OwnerUnit { get; set; }
+
+
+
+
+    }
+}
